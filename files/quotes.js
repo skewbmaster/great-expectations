f699583f087a33395486eb643ecd2b84f35030d4
skewbmaster <35039141+skewbmaster@users.noreply.github.com>
2020-01-31 16:36:19 +0000

    All the main files

---
+++ files/quotes.js
@@ -0,0 +1,7 @@
+function showCard() {
+    document.GetElementbyId(card) = white;
+}
+
+function showContent(n) {
+    document.GetElementbyId(content).displayText(url('https://imgur.com/' + n + 'gE6aCd1'))
+}
